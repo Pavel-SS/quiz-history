ada4298c1f162aa656e8c9f3b24d8aa4b33fd0a0
Pavel Sasnouski <sosnovskiy.paul@gmail.com>
2022-10-21 17:16:54 +0300

    Update Quiz.tsx

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useContext } from "react"
+import { useContext } from "react"
 import { Button } from "../../common/Button/Button"
 import { QuizContext } from "../../context/quiz"
 import { Question } from "../Question/Question"
@@ -6,13 +6,25 @@ import { Question } from "../Question/Question"
 
 export const Quiz = () => {
     const [quizState, dispatch] = useContext(QuizContext);
-    console.log('value: ' + quizState);
+    console.log('Quiz: ' + quizState);
+    const totalNumberQuestions = quizState.questions.length;
+    const currentQuestion = (quizState.currentQuestionIndex + 1);
     return(
         <>
             <div>
-                <div>Question 1/8</div>
-                <Question/>
-                <Button onClick={()=>dispatch({ type: 'Next_Question' })}>Next</Button>
+                {quizState.showResults && (
+                    <>
+                        <h2>Done</h2>
+                        <Button onClick={()=>dispatch({ type: 'Restart' })}>
+                            Restart
+                        </Button>
+                    </>
+                )}
+                {!quizState.showResults && (<>
+                    <div>Question {currentQuestion}/{totalNumberQuestions}</div>
+                    <Question/>
+                    <Button onClick={()=>dispatch({ type: 'Next_Question' })}>Next</Button>
+                </>)}
             </div>
         </>
     )
